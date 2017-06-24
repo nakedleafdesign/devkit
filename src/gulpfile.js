ee723d40851c7eecac0dc3b7511391ae5af42217
阪西功介 <nakedleafdesign@gmail.com>
2017-06-24 16:30:33 +0900

    gulpjsを編集

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,15 +1,15 @@
-  // ------------------------------------------------------------
+// ------------------------------------------------------------
 // require
 // ------------------------------------------------------------
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({
-        pattern: ['gulp-*', 'gulp.*'],
-        replaceString: /\bgulp[\-.]/
-    });
+  pattern: ['gulp-*', 'gulp.*'],
+  replaceString: /\bgulp[\-.]/
+});
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
+var del = require("del");
 var assetFunctions = require('node-sass-asset-functions');
-  var minifycss = require('gulp-minify-css');
 
 // ------------------------------------------------------------
 // config
@@ -25,59 +25,63 @@ var htmlDir = 'html/';
 
 var sassLintFlag = false;
 
+//========================================================================
+//config
+//========================================================================
+config = {
+  "name" : "devkit",
+  "path": {
+    "sharedSource" : "../",
+    "source": "./",
+    "dist": "../dist/",
+    "ejs": "ejs",
+    "sass": "/assets/scss",
+    "css": "/assets/css",
+    "img": "/assets/img",
+    "svg": "/assets/svg",
+    "html": "html",
+    "js": "/assets/js",
+    "file": "/assets/file",
+    "fonts": "/assets/fonts",
+    "bower" : "../bower_components/",
+    "dummy" : "/assets/dummy",
+    "node" : "node_modules/",
+    // "docs": "/docs",
+    // "dev": "/dev",
+  }
+};
 
-// ------------------------------------------------------------
-// scssコンパイル
-// ------------------------------------------------------------
+
+//========================================================================
+// scss コンパイルタスク
+//========================================================================
 
 var browsers = [
     '> 3%'
 ];
-gulp.task('scss', function(){
-    gulp.src(srcDir + assetsDir + 'scss/**/*.scss')
+
+gulp.task('scss', function(){``
+
+  var distDir = config.path.dist + config.path.css;
+
+    gulp.src(config.path.source + config.path.sass + '/**/*.scss')
         .pipe($.plumber({
             errorHandler: $.notify.onError("Error: <%= error.message %>")
         }))
         .pipe($.sourcemaps.init())
-        .pipe($.sass(
-            {
-              functions: assetFunctions({
-                // todo 治す
-                images_path:'./' + assetsDir + 'img',
-                http_images_path: "./assets/img"
-              })
-            }
-        ))
+        .pipe($.sass())
         .pipe($.postcss([
             // require('doiuse')({browsers: browsers}),
             // todo:ignoreする https://liginc.co.jp/206518
             require('autoprefixer')({browsers: browsers}),
             require('css-mqpacker')
         ]))
-        .pipe(minifycss())/*cssを圧縮*/
+        // ▼ 出力CSSを難読化させる場合はコメントアウトを外す
+        // .pipe($.csso())
         .pipe($.sourcemaps.write('./'))
+        .pipe(gulp.dest(distDir))
 
-        .pipe(gulp.dest(distDir +  assetsDir + 'css/'))
 });
-
-// ------------------------------------------------------------
-// scss lint
-// ------------------------------------------------------------
-
-gulp.task( 'scss-lint', function() {
-	return gulp.src(srcDir + assetsDir + 'scss/**/*.scss')
-    .pipe($.sassLint({
-      rules: {
-        'no-ids': 1,
-        'no-mergeable-selectors': 0
-      },
-      files: {ignore: srcDir + assetsDir + 'lib/**/*.scss'},
-      configFile: '.sass-lint.yml'
-    }))
-    .pipe($.sassLint.format())
-    .pipe($.sassLint.failOnError())
-} );
-
 
 // ------------------------------------------------------------
 // Hologram
@@ -93,7 +97,7 @@ gulp.task('hologram', function() {
 
 //--------------------------------[
 // var sass-lint = false;
-// 
+//
 // function sassLintFlag(flag){
 //     if (flag){
 //         sass-lint = true;
@@ -101,74 +105,102 @@ gulp.task('hologram', function() {
 //         sass-lint = false;
 //     }
 // }
-// 
-// 
+//
+//
 // gulp.task('watch', function () {
 //     sassLintFlag(false);
-//     
+//
 // });
-// 
+//
 // gulp.task('watch-lint', function () {
 //     sassLintFlag(true);
 //     gulp.watch(srcDir + assetsDir + 'scss/**/*.scss', ['sass','hologram','bs-reload']);
 // });
-//     
+//
 
-// ------------------------------------------------------------
+
+//========================================================================
 // @ js結合・圧縮
-// ------------------------------------------------------------
+//========================================================================
 
 gulp.task('js',function(){
-    // form
-    gulp.src(srcDir + assetsDir + 'js/form/lib/*.js')
-        .pipe($.concat('validate.js'))
+
+  var distDir = config.path.dist + config.path.js;
+
+    // jquery
+    gulp.src(config.path.source + config.path.node + 'jquery/dist/jquery.js')
         .pipe($.uglify())
-        .pipe(gulp.dest(distDir + assetsDir + 'js/form/'));
-    gulp.src(srcDir + assetsDir + 'js/form/*.js')
+        .pipe(gulp.dest(distDir));
+
+    // form.js
+    gulp.src([config.path.source + config.path.js + '/form/lib/*.js',config.path.source + config.path.js + '/form.js'])
         .pipe($.concat('form.js'))
-        .pipe(gulp.dest(distDir + assetsDir + 'js/form/'));
-    // vender
-    gulp.src(srcDir + assetsDir + 'js/vender/*.js')
+        .pipe($.uglify())
+        .pipe(gulp.dest(distDir));
+
+    //vender
+    gulp.src(config.path.source + config.path.node + 'flexibility/flexibility.js')
         .pipe($.concat('vender.js'))
         .pipe($.uglify())
-        .pipe(gulp.dest(distDir + assetsDir + 'js/'));
-    // jquery    
-    gulp.src(srcDir + assetsDir + 'js/_jquery.js')
-        .pipe($.concat('jquery.js'))
-        .pipe($.uglify())
-        .pipe(gulp.dest(distDir + assetsDir + 'js/'));
-    gulp.src( srcDir + assetsDir + 'js/!(_)*.js') //パーシャルを除外
-        .pipe($.uglify())
-        .pipe(gulp.dest(distDir + assetsDir + 'js/'))
-});
-//    gulp.src([srcDir + assetsDie + 'js/*.js','!' + srcDir + assetsDie + 'js/**/_*.js']) //パーシャルを除外
+        .pipe(gulp.dest(distDir));
 
-// ------------------------------------------------------------
+    //通常
+    gulp.src([config.path.source + config.path.js + '/!(_)*.js','!' + config.path.source + config.path.js + '/lib/*.js']) //パーシャルを除外
+        .pipe($.uglify())
+        .pipe(gulp.dest(distDir))
+});
+   // gulp.src([srcDir + assetsDie + 'js/*.js','!' + srcDir + assetsDie + 'js/**/_*.js']) //パーシャルを除外
+
+
+//========================================================================
 // @ $copy
-// ------------------------------------------------------------
+//========================================================================
+
+gulp.task('copy.html',function () {
+  var distDir = config.path.dist;
+
+  //html
+  gulp.src(config.path.source + config.path.html + '**/*.html')
+      .pipe(gulp.dest(distDir))
+      .pipe(browserSync.stream());
+
+});
 
 
 gulp.task('copy', function() {
-    gulp.src(srcDir + htmlDir + '**/*.html')
-        .pipe(gulp.dest(distDir))
+
+  var distDir = config.path.dist;
+
+
+    //img
+    gulp.src(config.path.source + config.path.img + '/**/*')
+        .pipe(gulp.dest(distDir + config.path.img))
         .pipe(browserSync.stream());
-    gulp.src(srcDir + assetsDir + 'img/**/*')
-        .pipe(gulp.dest(distDir + assetsDir +'img'))
+
+    //fonts
+    gulp.src(config.path.source + config.path.fonts + '/**/*')
+        .pipe(gulp.dest(distDir + config.path.fonts))
         .pipe(browserSync.stream());
-    gulp.src(srcDir + assetsDir + 'fonts/**/*')
-        .pipe(gulp.dest(distDir + assetsDir + 'fonts'))
+
+    //js library
+    gulp.src(config.path.source + config.path.js + '/lib/**/*')
+        .pipe(gulp.dest(distDir + config.path.js + '/lib/'))
         .pipe(browserSync.stream());
-    gulp.src('bower_components/font-awesome/fonts/**/*')
-      .pipe(gulp.dest(distDir + assetsDir + 'fonts'))
-      .pipe(browserSync.stream());
-    gulp.src(srcDir + assetsDir + 'js/lib/**/*')
-        .pipe(gulp.dest(distDir + assetsDir + 'js/lib/'))
+
+    //svg
+    gulp.src(config.path.source + config.path.svg + '/**/*')
+        .pipe(gulp.dest(distDir + config.path.svg))
+        .pipe(browserSync.stream());
+
+    //file
+    gulp.src(config.path.source + config.path.file + '/**/*')
+        .pipe(gulp.dest(distDir + config.path.file))
         .pipe(browserSync.stream());
 });
 
-// ------------------------------------------------------------
+//========================================================================
 // @ $browser-sync
-// ------------------------------------------------------------
+//========================================================================
 
 gulp.task('bs', function() {
     browserSync({
@@ -178,42 +210,61 @@ gulp.task('bs', function() {
     });
 });
 
-// ------------------------------------------------------------
+//========================================================================
 // @ $bs-reload
-// ------------------------------------------------------------
+//========================================================================
 
 gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
-// ------------------------------------------------------------
+
+//========================================================================
 // ejs
-// ------------------------------------------------------------
+//========================================================================
 var ejs = require("gulp-ejs");
-gulp.task("ejs", function() {
-    gulp.src( ["ejs/**/*.ejs",'!' + "ejs/**/_*.ejs"] )
-    .pipe($.plumber())
-       .pipe($.ejs())
-       .pipe($.rename({extname: '.html'}))
-        .pipe(gulp.dest(distDir)) 
+
+    var distDir = config.path.dist;
+    gulp.task("ejs", function () {
+      gulp.src([config.path.source + config.path.ejs + "/**/*.ejs",config.path.source + config.path.ejs + '!' + "/**/_*.ejs"])
+          .pipe($.plumber())
+          .pipe($.ejs())
+          .pipe($.rename({extname: '.html'}))
+          .pipe(gulp.dest(distDir))
 });
 
+
+//========================================================================
+// del : buildで使用
+//========================================================================
+gulp.task('clean', del.bind(null, ['../dist'],{ force:true }));
+
+
+// gulp.task('clean', function () {
+//   // del([
+//   //   config.path.dist,
+//   //   { force:false }
+//   // ]);
+// });
+
+//========================================================================
 // @ $watch
-// ------------------------------------------------------------
+//========================================================================
 
 gulp.task('watch', function () {
-    if(sassLintFlag){
-    gulp.watch(srcDir + assetsDir + 'scss/**/*.scss', ['scss','hologram','scss-lint','bs-reload']);
-    } else {
-    gulp.watch(srcDir + assetsDir + 'scss/**/*.scss', ['scss','hologram','bs-reload']);
-    }
-    gulp.watch(srcDir + assetsDir + 'js/**/*.js', ['js','bs-reload']);
-    gulp.watch(srcDir + assetsDir + 'html/**/*.html', ['copy','bs-reload']);
-    gulp.watch(srcDir + assetsDir + 'img/**/*.{png,jpg,jpeg,gif,svg,ico,cur}', ['copy','bs-reload']);
-    gulp.watch(srcDir + assetsDir + 'fonts/**/*.*', ['copy','bs-reload']);
-    gulp.watch(srcDir + 'ejs/**/*.*', ['ejs','bs-reload']);
-//     gulp.watch("../**/*.*").on('change', browserSync.reload);
+
+    gulp.watch(config.path.source + config.path.sass + '/**/*.scss', ['scss','hologram','bs-reload']);
+    gulp.watch(config.path.source + config.path.js + '/**/*.js', ['js','bs-reload']);
+    gulp.watch(config.path.source + config.path.html + '/**/*.html', ['copy','bs-reload']);
+    gulp.watch(config.path.source + config.path.ejs + '/**/*.*', ['ejs','bs-reload']);
+    
 });
+
+
+//========================================================================
+//========================================================================
+//========================================================================
+//========================================================================
 
 
 gulp.task('run', function (callback) {
@@ -221,4 +272,13 @@ gulp.task('run', function (callback) {
         'bs',
         'watch',
         callback);
+});
+
+
+gulp.task('build', function (callback) {
+  return runSequence(
+      'clean',
+      ['scss','hologram','js','ejs','copy.html','copy'],
+      callback
+  );
 });
