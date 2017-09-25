9f849abc8dc2ce760be1f787d3e487e6d2cba79a
阪西功介 <nakedleafdesign@gmail.com>
2017-09-25 22:07:09 +0900

    諸々修正

--- src/php/_partial/_key-visual.php
+++
@@ -1,9 +0,0 @@
-
-<section class="key-visual">
-  <div class="key-visual__inner">
-    <div class="key-visual__unit">
-      <h1 class="key-visual__title">default</h1>
-      <span class="key-visual__sub">default</span>
-    </div>
-  </div>
-</section>
